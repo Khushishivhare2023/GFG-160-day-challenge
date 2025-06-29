95f9c6692eb055ed48110e7caca382478414f0dd
Khushishivhare2023 <khushishivhare135@gmail.com>
2025-06-29 15:39:23 +0530

    commit

---
+++ searching/Day 31.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int peakElement(int[] arr) {
+        // code here
+        int n=arr.length;
+        
+        if(n==1){//if arr has only one element
+            return 0;
+        }
+        if(arr[0]>arr[1]){//boundary elements
+            return 0;
+        }
+        if(arr[n-1]>arr[n-2]){//boundary elements
+            return n-1;
+        }
+        int lo=1;
+        int hi=n-2;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(arr[mid]<arr[mid+1]){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return 0;
+    }
+}
